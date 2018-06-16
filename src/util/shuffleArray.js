45a45ef115935068cdc835357e3454020a742dae
cfdahill <cfdahill@gmail.com>
2018-06-16 14:12:20 -0700

    Updates

--- src/util/shuffleArray.js
+++
@@ -1,7 +0,0 @@
-export default function shuffleArray(array) {
-    for (let i = 0; i < array.length; i++) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
